f3ce4715a849df30845e9a92935f9d026c3e1da4
Marco A L Barbosa <malbarbo@gmail.com>
2024-02-21 16:28:43 -0300

    busca e arvores: adiciona algumas soluções para arvores binarias

---
+++ 07-busca-e-arvores/pratica-solucoes/cheia.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+
+@dataclass
+class No:
+    esq: Arvore
+    val: int
+    dir: Arvore
+
+
+Arvore = No | None
+
+
+def cheia(t: Arvore) -> bool:
+    r'''
+    Devolve True se *t* é uma árvore cheia, isto é, todos os nós tem grau 0 ou
+    2. Devolve False caso contrário.
+
+    Exemplos
+          t4  4
+            /   \
+         /        \
+    t2  8           6  t3
+      /   \       /
+     4  t1 7     5
+            \
+          t0 1
+
+    >>> t0 = No(None, 1, None)
+    >>> t1 = No(None, 7, No(None, 1, None))
+    >>> t2 = No(No(None, 4, None), 8, t1)
+    >>> t3 = No(No(None, 5, None), 6, None)
+    >>> t4 = No(t2, 4, t3)
+    >>> cheia(None)
+    True
+    >>> cheia(t4)
+    False
+    >>> cheia(t2)
+    False
+    >>> cheia(t1)
+    False
+    >>> t1.esq = No(None, 5, None)
+    >>> cheia(t1)
+    True
+    >>> cheia(t3)
+    False
+    >>> t3.dir = No(None, 5, None)
+    >>> cheia(t3)
+    True
+    >>> cheia(t2)
+    True
+    >>> cheia(t4)
+    True
+    '''
+    if t is None:
+        return True
+    else:
+        grau = int(t.esq is None) + int(t.dir is None)
+        return grau != 1 and cheia(t.esq) and cheia(t.dir)
---
+++ 07-busca-e-arvores/pratica-solucoes/maximo.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+
+@dataclass
+class No:
+    esq: Arvore
+    val: int
+    dir: Arvore
+
+
+Arvore = No | None
+
+
+def maximo(t: Arvore) -> int | None:
+    r'''
+    Devolve o valor máximo entre todos os elementos de *t* or None se *t* é
+    None.
+
+    Exemplos
+          t4  4
+            /   \
+         /         \
+    t2  8           6  t3
+      /   \       /
+     4  t1 7     5
+            \
+          t0 1
+
+    >>> t0 = No(None, 1, None)
+    >>> t1 = No(None, 7, No(None, 1, None))
+    >>> t2 = No(No(None, 4, None), 8, t1)
+    >>> t3 = No(No(None, 5, None), 6, None)
+    >>> t4 = No(t2, 4, t3)
+    >>> maximo(None) is None
+    True
+    >>> maximo(t0)
+    1
+    >>> maximo(t2)
+    8
+    >>> maximo(t3)
+    6
+    >>> maximo(t4)
+    8
+    '''
+    def _maximo(a: int | None, b: int | None) -> int | None:
+        if a is None:
+            return b
+        if b is None:
+            return a
+        else:
+            return max(a, b)
+    if t is None:
+        return None
+    else:
+        return _maximo(t.val, _maximo(maximo(t.esq), maximo(t.dir)))
